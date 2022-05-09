dc3fd5ead96c04825c21ee1e0723f5c1cdbd8e5c
Micah Newsum <micah.newsum@glidewelldental.com>
2022-05-09 08:46:29 -0700

    Create private inner Node class that for Doubly linked list.

--- Deque.java
+++ Deque.java
@@ -1,30 +1,48 @@
 import java.util.Iterator;
 
-/**
- * Doubly Linked List would allow for adding items at front and end of list in constnt time.
- * It would also allow for constant time removal from the front and back of list.
- * The Deque would also be initialized in constant time.
- * The size operation could provide constant time by keeping an instance variable that tracks
- * items that are added and removed.
- */
 public class Deque<Item> implements Iterable<Item> {
+    private Node<Item> first;
+    private Node<Item> last;
+    private int size;
 
-    // construct an empty deque
-    public Deque() {}
+    private class Node<Item> {
+        Node<Item> prev;
+        Node<Item> next;
+        Item item;
 
-    // is the deque empty?
+        Node() {
+            this.prev = null;
+            this.next = null;
+            this.item = null;
+        }
+    }
+
+    public Deque() {
+        this.first = new Node<Item>();
+        this.last = new Node<Item>();
+        this.size = 0;
+    }
+
     public boolean isEmpty() {
-        return false;
+        return (size == 0);
     }
 
     public int size() {
-        return 0;
+        return this.size;
     }
 
     public void addFirst(Item item) {
         if (item == null) {
             throw new IllegalArgumentException();
         }
+
+        Node<Item> newFirst = new Node<>();
+        newFirst.item = item;
+
+        Node<Item> oldFirst = this.first;
+
+        newFirst.next = oldFirst;
+        oldFirst.prev = newFirst;
     }
 
     public void addLast(Item item) {
@@ -46,14 +64,20 @@ public class Deque<Item> implements Iterable<Item> {
     }
 
     private class DequeIterator implements Iterator<Item> {
-        public boolean hasNext() {return false;}
+        public boolean hasNext() {
+            return false;
+        }
 
-        public void remove() {throw new UnsupportedOperationException();}
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
 
-        public Item next() {return null;}
+        public Item next() {
+            return null;
+        }
     }
 
     public static void main(String[] args) {
-        
+
     }
 }
