49b5bc2418ba43d57d7f7838bee0729be438e1db
Micah Newsum <micah.newsum@glidewelldental.com>
2022-05-18 13:33:18 -0700

    Add tests for removeFirst and removeLast.

--- DequeTest.java
+++ DequeTest.java
@@ -2,6 +2,8 @@ import org.junit.Test;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.util.NoSuchElementException;
+
 public class DequeTest {
 
     @Test(expected = IllegalArgumentException.class)
@@ -14,5 +16,17 @@ public class DequeTest {
     public void addLastThrowsIllegalArgumentException() {
         Deque<String> deque = new Deque<>();
         deque.addLast(null);
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void removeFirstThrowsNoSuchElementException() {
+        Deque<String> deque = new Deque<>();
+        deque.removeFirst();
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void removeLastThrowsNoSuchElementException() {
+        Deque<String> deque = new Deque<>();
+        deque.removeLast();
     }
 }
