bf44abc579d8b230bc69fd9b8c5327739480954e
Micah Newsum <micah.newsum@glidewelldental.com>
2022-05-18 13:25:58 -0700

    Create test.

--- DequeTest.java
+++ DequeTest.java
@@ -1,3 +1,12 @@
+import org.junit.Test;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 public class DequeTest {
-    
+
+    @Test(expected = IllegalArgumentException.class)
+    public void addFirstThrowsIllegalArgumentException() {
+        Deque<String> deque = new Deque<>();
+        deque.addFirst(null);
+    }
 }
