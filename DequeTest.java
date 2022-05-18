8dd3be1a10c7aa09b3a5c4eca7f2dceb762b6bcd
Micah Newsum <micah.newsum@glidewelldental.com>
2022-05-18 14:30:58 -0700

    Add iterator test.

--- DequeTest.java
+++ DequeTest.java
@@ -1,6 +1,5 @@
 import org.junit.Test;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 import java.util.Iterator;
 import java.util.NoSuchElementException;
@@ -43,5 +42,48 @@ public class DequeTest {
         Deque<String> deque = new Deque<>();
         Iterator<String> i = deque.iterator();
         i.remove();;
+    }
+
+    @Test
+    public void removeFirstTest() {
+        Deque<String> deque = new Deque<>();
+        deque.addFirst("My");
+        deque.addFirst("name");
+        deque.addFirst("is");
+        deque.addFirst("Micah");
+        deque.removeFirst();
+        deque.removeFirst();
+        deque.removeFirst();
+        deque.removeFirst();
+        assertEquals(0, deque.size());
+    }
+
+    @Test
+    public void removeLastTest() {
+        Deque<String> deque = new Deque<>();
+        deque.addFirst("My");
+        deque.addFirst("name");
+        deque.addFirst("is");
+        deque.addFirst("Micah");
+        deque.removeLast();
+        deque.removeLast();
+        deque.removeLast();
+        deque.removeLast();
+        assertEquals(0, deque.size());
+    }
+
+    @Test
+    public void testDequeIterator(){
+        Deque<String> deque = new Deque<>();
+        deque.addFirst("My");
+        deque.addFirst("name");
+        deque.addFirst("is");
+        deque.addFirst("Micah");
+        Iterator<String> i = deque.iterator();
+        String s = "";
+        while (i.hasNext()){
+            s = s + i.next() + " ";
+        }
+        assertEquals("Micah is name My ", s);
     }
 }
