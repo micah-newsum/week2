32d5bcbf21145fae12ccd33f51940e8448b12a86
Micah Newsum <micah.newsum@glidewelldental.com>
2022-05-19 12:25:34 -0700

    Fix generic array error.

--- RandomizedQueue.java
+++ RandomizedQueue.java
@@ -1,35 +1,59 @@
+import java.util.Iterator;
 
+public class RandomizedQueue<Item> implements Iterable<Item> {
+    private Object[] queue;
+    private int size;
 
-public class RandomizedQueue implements Iterable<Item> {
-    public RandomizedQueue(){
-
+    public RandomizedQueue() {
+        this.queue = new Object[10];
     }
 
     public boolean isEmpty() {
-
+        return (size == 0);
     }
 
     public int size() {
+        return size;
+    }
+
+    public void enqueue(Item item) {
 
     }
 
-    public void enqueue(Item item){
+    public Item dequeue() {
 
     }
 
-    public Item dequeue(){
+    // return a random item, but do not remove it
+    public Item sample() {
 
     }
 
-    public Item sample(){
-
+    public Iterator<Item> iterator() {
+        return new RandomizedQueueIterator(this.queue);
     }
 
-    public Iterator<Item> iterator(){
-
-    }
-
-    public static void main(String[] args){
+    private class RandomizedQueueIterator implements Iterator<Item> {
+        private Object[] queue;
         
+        private RandomizedQueueIterator(Object[] queue) {
+            // copy conents of queue to iterator's queue
+        }
+
+        public boolean hasNext() {
+
+        }
+
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+
+        public Item next() {
+
+        }
+    }
+
+    public static void main(String[] args) {
+
     }
 }
