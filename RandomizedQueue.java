4e30b827895512e1a3b256f1297ffa1662694e06
Micah Newsum <micah.newsum@glidewelldental.com>
2022-05-20 13:37:45 -0700

    Add more logic

--- RandomizedQueue.java
+++ RandomizedQueue.java
@@ -1,11 +1,18 @@
 import java.util.Iterator;
 
 public class RandomizedQueue<Item> implements Iterable<Item> {
-    private Object[] queue;
+    private Object[] items;
     private int size;
+    private int head;
+    private int tail;
+    private int capacity;
 
     public RandomizedQueue() {
-        this.queue = new Object[10];
+        this.capacity = 10;
+        this.items = new Object[capacity];
+        this.head = 0;
+        this.tail = 0;
+        this.size = 0;
     }
 
     public boolean isEmpty() {
@@ -17,27 +24,42 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
     }
 
     public void enqueue(Item item) {
-
+        if (tail == capacity) { 
+            // double array size
+        }
+        items[tail] = item;
+        tail++;
     }
 
     public Item dequeue() {
+        // check if head is 1/4th size of capacity
 
+        Item item = (Item) items[head];
+        items[head] = null;
+        head++;
+        return item;
     }
 
     // return a random item, but do not remove it
     public Item sample() {
-
+        return (Item) items[0];
     }
 
     public Iterator<Item> iterator() {
-        return new RandomizedQueueIterator(this.queue);
+        return new RandomizedQueueIterator<Item>();
     }
 
-    private class RandomizedQueueIterator implements Iterator<Item> {
-        private Object[] queue;
+    private class RandomizedQueueIterator<Item> implements Iterator<Item> {
+        private Object[] iteratorItems;
         
-        private RandomizedQueueIterator(Object[] queue) {
+        private RandomizedQueueIterator() {
             // copy conents of queue to iterator's queue
+            iteratorItems = new Object[tail - head];
+            int iteratorIndex = 0;
+            for (int i = head; i <= tail; i++) {
+                iteratorItems[iteratorIndex] = items[i];
+                iteratorIndex++;
+            }
         }
 
         public boolean hasNext() {
